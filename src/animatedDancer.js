44d058ea2fa58ebd6cfdc5579f94ae1ee8fd32de
francoabaroa <franco9395@gmail.com>
2016-09-20 12:36:09 -0700

    changed effects for animatedDancer

--- src/animatedDancer.js
+++ src/animatedDancer.js
@@ -6,9 +6,13 @@ animatedDancer.prototype = Object.create(makeDancer.prototype);
 
 animatedDancer.prototype.constructor = animatedDancer;
 
-animatedDancer.prototype.slideUp = function() {
+animatedDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.slideUp();
+  this.$node.animate({
+    opacity: '0.8',
+    height: '150px',
+    width: '150px'
+  }).toggle();
 };
 
 
