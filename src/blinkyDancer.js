8ce15db357a4edb7fae004ebca76774e1ed6e09c
francoabaroa <franco9395@gmail.com>
2016-09-20 12:36:48 -0700

    modified special effects for blinkydancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  // this.$node = $('<span class="test"></span>');
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
