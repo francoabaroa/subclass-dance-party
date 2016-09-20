74b648dac6f4d1a13f75e099b61f8cac80159e14
francoabaroa <franco9395@gmail.com>
2016-09-19 17:13:29 -0700

    started to refactor to pseudoclassical style

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,10 +1,11 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
   var oldStep = blinkyDancer.step;
+  // console.log(oldStep);
 
   blinkyDancer.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
@@ -14,7 +15,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
     // other effects you can use on a jQuery-wrapped html tag.
     blinkyDancer.$node.toggle();
   };
-
+  // console.log(blinkyDancer.step);
   return blinkyDancer;
 };
 
