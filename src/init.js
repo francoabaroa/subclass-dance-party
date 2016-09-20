c2d31080681fea9e36b757241140c0865e28a054
francoabaroa <franco9395@gmail.com>
2016-09-19 20:11:04 -0700

    minor changes

--- src/init.js
+++ src/init.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
      */
 
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    console.log(dancerMakerFunctionName);
     
 
     // get the maker function for the kind of dancer we're supposed to make
