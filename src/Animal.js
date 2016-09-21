3f3c7dabcd4bd5bbe53c4c9fee5f53a4d9ed0dc6
francoabaroa <franco9395@gmail.com>
2016-09-20 17:06:46 -0700

    modified filename

---
+++ src/Animal.js
@@ -0,0 +1,31 @@
+var Animal = function(top, left, timeBetweenSteps) {
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
+
+  
+  if (this instanceof ButterflyDancer) {
+    this.$node = $('<span class="butterfly"></span>');
+  } else if (this instanceof DogDancer) {
+    this.$node = $('<span class="lameDog"></span>');
+  } else if (this instanceof CatDancer) {
+    this.$node = $('<span class="cat"></span>');
+  } else {
+    this.$node = $('<span class="dancer"></span>');
+  }
+  
+  this.step();
+  this.setPosition(top, left);
+};
+
+Animal.prototype.step = function() {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+Animal.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
