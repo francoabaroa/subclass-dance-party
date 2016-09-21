46047223f83c10ffc4929ad6bca63dd25136e8e8
francoabaroa <franco9395@gmail.com>
2016-09-20 17:07:07 -0700

    modified filename

---
+++ src/ButterflyDancer.js
@@ -0,0 +1,15 @@
+var ButterflyDancer = function(top, left, timeBetweenSteps) {
+  Animal.call(this, top, left, timeBetweenSteps);
+};
+
+ButterflyDancer.prototype = Object.create(Animal.prototype);
+
+ButterflyDancer.prototype.constructor = ButterflyDancer;
+
+ButterflyDancer.prototype.step = function() {
+  Animal.prototype.step.call(this);
+};
+
+
+
+
---
+++ src/CatDancer.js
@@ -0,0 +1,19 @@
+var CatDancer = function(top, left, timeBetweenSteps) {
+  Animal.apply(this, arguments);
+};
+
+CatDancer.prototype = Object.create(Animal.prototype);
+
+CatDancer.prototype.constructor = CatDancer;
+
+CatDancer.prototype.step = function() {
+  Animal.prototype.step.call(this);
+};
+
+
+
+
+
+
+
+
